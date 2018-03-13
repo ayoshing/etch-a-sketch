481eba73d7063366d0192f20f5ebb854ee434b8d
shing <shing2487@gmail.com>
2018-03-13 15:27:37 -0400

    updated grid style to remove border and include background color and created function to calculate grid size based on user input

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const canvas = document.querySelector('.canvas');
 const reset = document.getElementById('reset');
+var gridSize = '';
+
+function calcGridSize(gridNum) {
+  gridSize = (600 / (Number(gridNum))) + 'px';
+  createGrid();
+}
 
 function createGrid() {
   var div = document.createElement('div');
-  div.style.width = '20px';
-  div.style.height = '20px';
-  div.style.border = 'solid 1px black';
+  div.style.width = gridSize;
+  div.style.height = gridSize;
+  div.style.background = '#dbdbdb';
   canvas.appendChild(div);
 }
 
-createGrid();
+calcGridSize(prompt('Enter canvas size:', '16'));
