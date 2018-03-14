a20e32f95b43b54ac459c2e50df3beed0d370835
shing <shing2487@gmail.com>
2018-03-14 11:30:04 -0400

    removed moz focus dotted line, created default canvas, and added reset button function.

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const canvas = document.querySelector('.canvas');
 const reset = document.getElementById('reset');
 var gridSize = '';
 
-function createCanvas(gridNum) {
-  gridSize = (600 / (Number(gridNum))) + 'px';
-  canvas.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
-  for (i = 0; i < (Math.pow(gridNum, 2)); i++) {
-    createGrid();
+function clearCanvas() {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
   }
 }
 
@@ -20,4 +18,36 @@ function createGrid() {
     div.style.background = 'black';});
 }
 
-createCanvas(prompt('Enter canvas size:', '16'));
+function createCanvas(gridNum) {
+  clearCanvas();
+  gridSize = (600 / (Number(gridNum))) + 'px';
+  canvas.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+  for (i = 0; i < (Math.pow(gridNum, 2)); i++) {
+    createGrid();
+  }
+}
+
+function userInput() {
+  createCanvas(prompt('Enter canvas size:', '16'));
+}
+
+function defaultGrid() {
+  var div = document.createElement('div');
+  div.style.width = '37.5px';
+  div.style.height = '37.5px';
+  div.style.background = '#dbdbdb';
+  canvas.appendChild(div);
+  div.addEventListener('mouseover', function () {
+    div.style.background = 'black';});
+}
+
+function main() {
+  canvas.style.gridTemplateColumns = 'repeat(16, 1fr)';
+  for (i = 0; i < 256; i++) {
+    defaultGrid();
+  }
+
+  reset.addEventListener('click', userInput);
+}
+
+main();
